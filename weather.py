6ba98c7b5f32d5e849d08cbec1de54dc8e703c9a
Md. Afzal Hassan Ehsani <iemafzalhassan@gmail.com>
2024-02-20 08:57:55 +0530

    Weather App

---
+++ weather.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import tkinter as tk
+from geopy.geocoders import Nominatim
+from tkinter import ttk,messagebox
+from timezonefinder import TimezoneFinder
+from datetime import *
+import requests
+import pytz
+from PIL import Image, ImageTk
